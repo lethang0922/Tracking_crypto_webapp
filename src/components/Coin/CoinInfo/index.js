025aa4f2620380507f3015462e8e19c8a09b2109
lethang0922 <le.thang0922@gmail.com>
2024-06-30 17:34:51 -0700

    set the anchor tags as a string

--- src/components/Coin/CoinInfo/index.js
+++ src/components/Coin/CoinInfo/index.js
@@ -4,7 +4,9 @@ function CoinInfo({ heading, desc }) {
   return (
     <div className='grey-wrapper'>
       <h2 className='coin-info-heading'> {heading}</h2>
-      <p className='coin-info-desc'>{desc}</p>
+      {/* Set the anchor tags as a link */}
+      <p className='coin-info-desc'
+        dangerouslySetInnerHTML={{ __html: desc }}></p>
     </div>
   )
 }
